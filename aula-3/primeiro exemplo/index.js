abb3daecb45ecfecc061a208f1123e811bd02031
Matheus A. Haas <matheus.haas@icloud.com>
2024-09-18 16:58:16 -0300

    created: directory for study

---
+++ aula-3/primeiro exemplo/index.js
@@ -0,0 +1,7 @@
+let numeroAleatorio = prompt("Informe um número aleatório")
+
+if (numeroAleatorio > 1000){
+    alert("Número acima de 1000 digitado!")
+} else {
+    alert("Numero digitado não é maior que 1000.")
+}
