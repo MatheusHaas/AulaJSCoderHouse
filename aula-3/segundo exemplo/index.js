a69868e35454369c894af2af748f502b2d41a0af
Matheus A. Haas <matheus.haas@icloud.com>
2024-09-18 17:31:43 -0300

    change: add more code and sub-directories

---
+++ aula-3/segundo exemplo/index.js
@@ -0,0 +1,7 @@
+let palavra = prompt("Informe uma palavra")
+
+if((palavra == "Olá") || (palavra == "Ola")){
+    console.log("Você esta tentando conversar com uma página.")
+} else {
+    alert("Talvez precise informar algo diferente!")
+}
---
+++ aula-3/terceiro exemplo/index.js
@@ -0,0 +1,8 @@
+let numeroRandom = prompt("Digite qualquer número")
+let numeroInteiro = parseInt(numeroRandom)
+
+if(numeroInteiro >= 10 && numeroInteiro <= 50){
+    alert("Você conseguiu entrar na condição!")
+}else{
+    alert("Tente novamente.")
+}
